d4f3d1a767ef49d1ab7ec330f4adda84c15d62c5
EdenGoforIt <79078528+EdenGoforIt@users.noreply.github.com>
2022-09-22 07:55:00 +1200

    1 file modified

--- src/app/shared/dynamic-form/base-form.directive.ts
+++ src/app/shared/dynamic-form/base-form.directive.ts
@@ -1,14 +1,21 @@
-import { Directive, Input, OnInit } from "@angular/core";
-import { Field } from "../models/field";
+import { Directive, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Field } from '../models/field';
 @Directive({
-  selector:'base-form'
+  selector: 'base-form',
 })
-export class BaseFormDirective implements OnInit{
-
+export class BaseFormDirective {
   @Input() field: Field<any>;
+  @Input() value: any;
 
-  ngOnInit(){
+  @Output() clear = new EventEmitter<any>();
+  @Output() valueChange = new EventEmitter<any>();
 
+  get val(): any {
+    return this.value;
   }
 
+  set val(val: any) {
+    this.value = val;
+    this.valueChange.emit(this.value);
+  }
 }
